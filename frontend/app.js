ea29bae6a2ed169f0c52c6bbb2441a2605cbff4e
adelchi <streamsalesforce@gmail.com>
2026-02-15 16:51:09 +0100

    removed log

--- frontend/app.js
+++ frontend/app.js
@@ -21,7 +21,6 @@ form.addEventListener("submit", async (e) => {
 
     try {
 					const res = await fetch("/generate_question", {
-        		console.log(res)
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(payload)
