189980693bd5fbd6c756c59e0ff191102fa2952b
adelchi <streamsalesforce@gmail.com>
2026-02-15 16:15:11 +0100

    debug app.js

--- frontend/app.js
+++ frontend/app.js
@@ -21,7 +21,7 @@ form.addEventListener("submit", async (e) => {
 
     try {
 					const res = await fetch("/generate_question", {
-//        const res = await fetch("http://127.0.0.1:5000/generate_question", {
+        		console.log(res)
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(payload)
