e200a8bd9874c58487e4d261e834dd05dae3dd15
adelchi <streamsalesforce@gmail.com>
2026-02-15 19:23:20 +0100

    openai key hardcoded for testing

--- backend/llm/call_llm.py
+++ backend/llm/call_llm.py
@@ -25,9 +25,8 @@ class CallLLM:
 
         if not self.mock:
             # Read API key from environment
-            api_key = "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"
-            # api_key = os.getenv("OPENAI_API_KEY")
-            # print("OPENAI_API_KEY:", os.getenv("OPENAI_API_KEY"))
+            api_key = os.getenv("openai_api_key")
+            print("OPENAI_API_KEY:", os.getenv("openai_api_key"))
 
             if not api_key:
                 raise ValueError(
